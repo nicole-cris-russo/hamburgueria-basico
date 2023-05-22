b4735c4e80fac7743308531faf38a614f09960da
Nicole Russo <nicolecrst@gmail.com>
2023-05-22 15:27:30 -0400

    refactor: clearing console.log

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -8,8 +8,6 @@ export const Input = ({ setValueInput }) => {
     setValueInput(value)
   }
 
-  const value = watch("value", "")
-  console.log(value)
   return (
     <Form onSubmit={handleSubmit(submitData)}>
       <input 
